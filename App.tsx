3382d419fd919ce208e6930b726803ac8de40002
João Vitor Sampaio <joaovsampaio15@gmail.com>
2022-11-24 21:07:50 -0300

    tema armazenado em cache

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useFonts } from "expo-font";
 import { ThemeProvider } from "styled-components/native";
 import { light, dark } from "./app/themes/Theme.styled";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -14,24 +15,50 @@ import QuizNormal from "./app/components/quiz/QuizNormal";
 import QuizHard from "./app/components/quiz/QuizHard";
 import GameOver from "./app/components/GameOver";
 import ModalCustom from "./app/UI/ModalCustom";
+import { Alert } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [selectedTheme, setSelectedTheme] = useState(dark);
+
+  const storeTheme = async () => {
+    try {
+      await AsyncStorage.setItem("@theme", selectedTheme.name);
+    } catch (error) {
+      Alert.alert(`${error}`);
+    }
+  };
+
+  const getTheme = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@theme");
+      if (value !== null) {
+        setSelectedTheme(value === "dark" ? light : dark);
+      }
+    } catch (error) {
+      Alert.alert(`${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getTheme();
+  }, [getTheme]);
+
+  const HandleThemeChange = () => {
+    setSelectedTheme(selectedTheme.name === "dark" ? light : dark);
+
+    storeTheme();
+  };
+
   const [fontsLoaded] = useFonts({
     RobotoBold: require("./app/assets/fonts/Roboto-Bold.ttf"),
     RobotoLight: require("./app/assets/fonts/Roboto-Light.ttf"),
   });
 
-  const [selectedTheme, setSelectedTheme] = useState(dark);
-
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-
-  const HandleThemeChange = () => {
-    setSelectedTheme(selectedTheme.name === "dark" ? light : dark);
-  };
 
   return (
     <ThemeProvider theme={selectedTheme}>
---
+++ README.md
@@ -0,0 +1,15 @@
+# QuizBandeira :world_map: :question:
+
+O QuizBandeira é um desafio, onde o usuário deve acertar dentre 4 opções a bandeira correta.
+
+## O projeto utiliza:
+
+- React Native
+- Styled-Components
+- React Navigation
+
+## Imagens :arrow_down:
+
+![Imagem QuizBandeira 1](<assets/img_git/img_git(1).PNG>)
+
+![Imagem QuizBandeira 2](<assets/img_git/img_git(2).PNG>)
